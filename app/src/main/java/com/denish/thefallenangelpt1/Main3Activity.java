c5287e80b66b9567076a026246833e5517947ae6
Denish Goklani <golug24@gmail.com>
2020-02-13 20:55:22 +0530

    Added Transparent Status bar

--- app/src/main/java/com/denish/thefallenangelpt1/Main3Activity.java
+++ app/src/main/java/com/denish/thefallenangelpt1/Main3Activity.java
@@ -1,7 +1,9 @@
 package com.denish.thefallenangelpt1;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Main3Activity extends AppCompatActivity {
@@ -9,5 +11,9 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
     }
 }
